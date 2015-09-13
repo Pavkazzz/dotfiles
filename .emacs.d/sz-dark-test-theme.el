a3e32b4af572118799ed2e8ed04eec0dffe3dbde
pavka <pavkazzz@mail.ru>
2015-09-13 10:59:33 +0000

    first commit

---
+++ .emacs.d/sz-dark-test-theme.el
@@ -0,0 +1,12 @@
+(deftheme sz-dark-test
+  "Created 2015-07-04.")
+
+(custom-theme-set-variables
+ 'sz-dark-test
+ '(custom-safe-themes (quote ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "382cc6b38d34f94b91cf482e2ffec7ca160d5739d3e86d062b6efea5902311d4" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default))))
+
+(custom-theme-set-faces
+ 'sz-dark-test
+ )
+
+(provide-theme 'sz-dark-test)
---
+++ .vimrc
@@ -0,0 +1,5 @@
+syntax on
+filetype plugin indent on
+execute pathogen#infect()
+set background=dark
+colorscheme solarized
